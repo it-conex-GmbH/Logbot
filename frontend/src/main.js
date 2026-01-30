4be9e59f90be36387007492b99f5d11927b2b120
Philipp Fischer <p.fischer@itconex.de>
2026-01-30 18:17:59 +0100

    main.js kein port css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './assets/css/main.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
